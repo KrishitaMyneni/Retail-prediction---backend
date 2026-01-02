1c3832f3babd3b9a2f840f9df4d8ede2de5f347e
KrishitaMyneni <krishitamyneni5@gmail.com>
2026-01-02 20:01:36 +0530

    fix render port - 1

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_cors import CORS
 import joblib
 import pandas as pd
+import os
 
 app = Flask(__name__)
 CORS(app)  # 🔥 THIS LINE FIXES THE ERROR
@@ -35,4 +36,5 @@ def predict():
     return f"Predicted sales: {prediction}"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
